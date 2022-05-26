0f2201d9753cbc53b8d06e34b126776567414c78
DevUnusual <carlos.meneses.g.s@gmail.com>
2022-05-25 23:19:41 -0300

    swagger configured in the aplication

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swagger from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerJSON from "../swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerJSON));
 
 app.use("/users", usersRoutes);
 
